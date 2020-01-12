54f6d34e31f489c963fd93dd5a282a4d25eaa46c
Aviral Khare <aviralkhare5@gmail.com>
2020-01-12 22:58:28 +0530

    updated excel report for metro card

--- src/main/java/org/metro/utility/CreateCardReport.java
+++ src/main/java/org/metro/utility/CreateCardReport.java
@@ -5,6 +5,7 @@ import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.List;
 
+import org.apache.poi.ss.usermodel.BorderStyle;
 import org.apache.poi.ss.usermodel.Cell;
 import org.apache.poi.ss.usermodel.CellStyle;
 import org.apache.poi.ss.usermodel.Font;
@@ -30,41 +31,35 @@ public class CreateCardReport {
 
 			// Create a Sheet
 			Sheet sheet = workbook.createSheet("Metro");
-
-			// Create a Font for styling header cells
-			Font headerFont = workbook.createFont();
-			headerFont.setBold(true);
-			headerFont.setFontHeightInPoints((short) 14);
-			headerFont.setColor(IndexedColors.BLACK.getIndex());
-
-			// Create a CellStyle with the font
-			CellStyle headerCellStyle = workbook.createCellStyle();
-			headerCellStyle.setFont(headerFont);
+			
 			Integer i = 1;
 			Row metroId = sheet.createRow(i++);
 			Cell cell1 = metroId.createCell(0);
 			cell1.setCellValue("Metro Card Id");
-			cell1.setCellStyle(headerCellStyle);
+			cell1.setCellStyle(setCellStyle(workbook,0));
 
 			Cell cell2 = metroId.createCell(1);
 			cell2.setCellValue(metroCard.getId());
+			cell2.setCellStyle(setCellStyle(workbook,1));
 
 			Row userName = sheet.createRow(i++);
 			Cell cell3 = userName.createCell(0);
 			cell3.setCellValue("Metro User Name");
-			cell3.setCellStyle(headerCellStyle);
+			cell3.setCellStyle(setCellStyle(workbook,0));
 
 			Cell cell4 = userName.createCell(1);
 			cell4.setCellValue(metroCard.getName());
+			cell4.setCellStyle(setCellStyle(workbook,1));
 
 			Row balance = sheet.createRow(i++);
 			Cell cell5 = balance.createCell(0);
 			cell5.setCellValue("Metro Card Balance");
-			cell5.setCellStyle(headerCellStyle);
+			cell5.setCellStyle(setCellStyle(workbook,0));
 
 			Cell cell6 = balance.createCell(1);
 			cell6.setCellValue(metroCard.getBalance());
-
+			cell6.setCellStyle(setCellStyle(workbook,1));
+			
 			sheet.createRow(i++);
 			sheet.createRow(i++);
 
@@ -73,7 +68,7 @@ public class CreateCardReport {
 			for (int j = 0; j < columns.length; j++) {
 				Cell cell = headerRow.createCell(k++);
 				cell.setCellValue(columns[j]);
-				cell.setCellStyle(headerCellStyle);
+				cell.setCellStyle(setCellStyle(workbook,0));
 			}
 
 			for (Journey journey : journeyList) {
@@ -81,18 +76,23 @@ public class CreateCardReport {
 				k = 2;
 				Cell source = row.createCell(k++);
 				source.setCellValue(journey.getSource().toString());
+				source.setCellStyle(setCellStyle(workbook,1));
 
 				Cell swipeIn = row.createCell(k++);
 				swipeIn.setCellValue(journey.getSwipeIn().toString());
+				swipeIn.setCellStyle(setCellStyle(workbook,1));
 
 				Cell destination = row.createCell(k++);
 				destination.setCellValue(journey.getDestination().toString());
+				destination.setCellStyle(setCellStyle(workbook,1));
 
 				Cell swipeOut = row.createCell(k++);
 				swipeOut.setCellValue(journey.getSwipeOut().toString());
+				swipeOut.setCellStyle(setCellStyle(workbook,1));
 
 				Cell fare = row.createCell(k++);
 				fare.setCellValue(journey.getFare().toString());
+				fare.setCellStyle(setCellStyle(workbook,1));
 			}
 
 			for (i = 0; i < 7; i++) {
@@ -111,5 +111,21 @@ public class CreateCardReport {
 			workbook.close();
 
 		}
+	}
+	
+	private static CellStyle setCellStyle(Workbook workbook,Integer i) {
+		CellStyle cellStyle = workbook.createCellStyle();
+		if(i == 0) {
+			Font headerFont = workbook.createFont();
+			headerFont.setBold(true);
+			headerFont.setFontHeightInPoints((short) 14);
+			headerFont.setColor(IndexedColors.BLACK.getIndex());
+			cellStyle.setFont(headerFont);
+		}
+			cellStyle.setBorderTop(BorderStyle.MEDIUM);
+			cellStyle.setBorderBottom(BorderStyle.MEDIUM);
+			cellStyle.setBorderLeft(BorderStyle.MEDIUM);
+			cellStyle.setBorderRight(BorderStyle.MEDIUM);
+		return cellStyle;
 	}
 }
