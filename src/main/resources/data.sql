a038594a6708dc9770cc7b5ac90b99a8458d0752
Aviral Khare <aviralkhare5@gmail.com>
2020-01-12 23:45:15 +0530

    updated initial data

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,5 +1,8 @@
  
 INSERT INTO METROCARD (name, balance) VALUES
-  ('Shahrukh Khan', 100),
-  ('Salman Khan', 200),
-  ('Aamir Khan', 300);
+  ('Shahrukh Khan', 250),
+  ('Salman Khan', 300),
+  ('Aamir Khan', 150),
+  ('Hrithik Roshan', 180),
+  ('Akshay Kumar', 240),
+  ('Ajay Devgn', 160);
